f38714f3bd39f5ddca23facd7f0b89833d0b2d89
lord-hellgrim <hallgrimurv@gmail.com>
2023-12-05 12:09:28 +0000

    day 1 solved in C

--- 2023/day_1/C/puzzle2.c
+++ 2023/day_1/C/puzzle2.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
+
+void reverse(char *str1)  
+{  
+    // declare variable  
+    int i, len, temp;  
+    len = strlen(str1); // use strlen() to get the length of str string  
+      
+    // use for loop to iterate the string   
+    for (i = 0; i < len/2; i++)  
+    {  
+        // temp variable use to temporary hold the string  
+        temp = str1[i];  
+        str1[i] = str1[len - i - 1];  
+        str1[len - i - 1] = temp;  
+    }  
+}
+
+int strindex(char* string, char* substring) {
+    char* loc = strstr(string, substring);
+    if (loc != NULL) {
+        int result = loc - string;
+        return result;
+    } else {
+        return 10000;
+    }
+}
 
 int main() {
     FILE *file;
@@ -12,6 +39,12 @@ int main() {
     // Open the file in read mode
     file = fopen("puzzle_input.txt", "r");
 
+    // Check if the file was opened successfully
+    if (file == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+
     // Get the size of the file
     fseek(file, 0, SEEK_END);
     file_size = ftell(file);
@@ -20,312 +53,149 @@ int main() {
     // Allocate memory for the buffer to hold the file content
     buffer = malloc(sizeof(char) * (file_size + 1));
 
+    // Check if the memory was allocated successfully
+    if (buffer == NULL) {
+        perror("Error allocating memory");
+        fclose(file);
+        return -1;
+    }
+
     // Read the file into the buffer
     fread(buffer, sizeof(char), file_size, file);
     buffer[file_size] = '\0'; // Null-terminate the string
 
     int i = 0;
-    int first_index = 0;
-    int second_index = 0;
-    int first_index_set = 0;
+    int first_num = 10000;
+    int last_num = 0;
+    int first_num_index = 10000;
+    int last_num_index = 0;
+
+    int first_text = 10000;
+    int last_text_index = 0;
+    int first_text_index = 10000;
+    int last_text = 0;
+
+    int final_first = 0;
+    int final_last = 0;
     int total_sum = 0;
-    char tester[10];
-    int tester_index = 0;
+
+    char* one = "one";
+    char* two = "two";
+    char* three = "three";
+    char* four = "four";
+    char* five = "five";
+    char* six = "six";
+    char* seven = "seven";
+    char* eight = "eight";
+    char* nine = "nine";
+    char* zero = "zero";
+
+    char* eno = "eno";
+    char* owt = "owt";
+    char* eerht = "eerht";
+    char* ruof = "ruof";
+    char* evif = "evif";
+    char* xis = "xis";
+    char* neves = "neves";
+    char* thgie = "thgie";
+    char* enin = "enin";
+    char* orez = "orez";
+    char* delim = "\n";
         
-    while (i< file_size) {
-        if (buffer[i] == '\n') {
-            if (second_index == 20) {
-                int middle = (first_index-48)*10 + (first_index-48);
-                printf("midsum: %d\n", middle);
-                total_sum += middle;
-            } else {
-                int middle = (first_index-48)*10 + (second_index-48);
-                printf("midsum: %d\n", middle);
-                total_sum += middle;
+    char* token = strtok(buffer, delim);
+    while (token) {
+
+        first_num_index = 10000;
+        first_text_index = 10000;
+        last_num_index = 10000;
+        last_text_index = 10000;
+
+        long long positions[10] = {1000};
+        long long positions_reverse[10] = {1000};
+
+        positions[1] = strindex(token, one);
+        positions[2] = strindex(token, two);
+        positions[3] = strindex(token, three);
+        positions[4] = strindex(token, four);
+        positions[5] = strindex(token, five);
+        positions[6] = strindex(token, six);
+        positions[7] = strindex(token, seven);
+        positions[8] = strindex(token, eight);
+        positions[9] = strindex(token, nine);
+        positions[0] = strindex(token, zero);
+
+        int i = 0;
+        int min = 10000;
+        while (i<10) {
+            if (positions[i] < first_text_index) {
+                min = i;
+                first_text_index = positions[i];
             }
-            first_index_set = 0;
-            first_index = 0;
-            second_index = 20;
-        } else if (buffer[i] >= 48 && buffer[i] <= 57) {
-            memset(tester, 0, strlen(tester));
-            if (first_index_set == 0) {
-                first_index = buffer[i];
-                first_index_set = 1; 
-            } else {
-                second_index = buffer[i];
-            }
-        } else if (tester_index == 0) {
-            first_char:
-            switch (buffer[i])
-            {
-            case 'o':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 't':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'f':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 's':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'e':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'n':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'z':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            default:
-                memset(tester, 0, strlen(tester));
-                tester_index = 0;
-                tester[tester_index] = buffer[i];
+            i++;
+        }
+        first_text = min;
+
+        int j = 0;
+        while (j < strlen(token)) {
+            if (token[j] >= 48 && token[j] <= 57) {
+                first_num = token[j] - 48;
+                first_num_index = j;
                 break;
             }
-        } else if (tester_index == 1) {
-            switch (buffer[i])
-            {
-            case 'n':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'w':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'h':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'o':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'i':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'e':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            default:
-                memset(tester, 0, strlen(tester));
-                tester_index = 0;
-                goto first_char;
-                break;
-            }
-        } else if (tester_index == 2) {
-            switch (buffer[i])
-            {
-            case 'e':
-                if (first_index_set == 0) {
-                    first_index = 1 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                    first_index_set = 1;
-                } else {
-                    second_index = 1 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                }
-                break;
-            case 'o':
-                if (first_index_set == 0) {
-                    first_index = 2 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                    first_index_set = 1;
-                } else {
-                    second_index = 2 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                }        
-                break;
-            case 'r':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'u':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'v':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'x':
-                if (first_index_set == 0) {
-                    first_index = 6 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                    first_index_set = 1;
-                } else {
-                    second_index = 6 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                }
-                break;
-            case 'g':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'n':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            default:
-                memset(tester, 0, strlen(tester));
-                tester_index = 0;
-                goto first_char;
-                break;
-            }
-        } else if (tester_index == 3) {
-            switch (buffer[i])
-            {
-            case 'e':
-                if (tester[0] == 'f') {
-                    if (first_index_set == 0) {
-                        first_index = 5 + 48;
-                        tester_index = 0;
-                        memset(tester, 0, strlen(tester));
-                        first_index_set = 1;
-                    } else {
-                        second_index = 5 + 48;
-                        tester_index = 0;
-                        memset(tester, 0, strlen(tester));
-                    }
-                } else if (tester[0] == 'n') {
-                    if (first_index_set == 0) {
-                        first_index = 9 + 48;
-                        tester_index = 0;
-                        memset(tester, 0, strlen(tester));
-                        first_index_set = 1;
-                    } else {
-                        second_index = 9 + 48;
-                        tester_index = 0;
-                        memset(tester, 0, strlen(tester));
-                    }
-                } else {
-                    tester[tester_index] = buffer[i];
-                    tester_index++;
-                }
-                break;
-            case 'r':
-                if (first_index_set == 0) {
-                    first_index = 4 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                    first_index_set = 1;
-                } else {
-                    second_index = 4 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                }
-                break;
-            case 's':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'h':
-                tester[tester_index] = buffer[i];
-                tester_index++;
-                break;
-            case 'o':
-                if (first_index_set == 0) {
-                    first_index = 0 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                    first_index_set = 1;
-                } else {
-                    second_index = 0 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                }     
-                break;
-            default:
-                memset(tester, 0, strlen(tester));
-                tester_index = 0;
-                goto first_char;
-                break;
-            }
-        } else if (tester_index == 4) {
-            switch (buffer[i])
-            {
-            case 'e':
-                if (first_index_set == 0) {
-                    first_index = 3 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                    first_index_set = 1;
-                } else {
-                    second_index = 3 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                }
-                break;
-            case 'n':
-                if (first_index_set == 0) {
-                    first_index = 7 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                    first_index_set = 1;
-                } else {
-                    second_index = 7 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                }         
-                break;
-            case 't':
-                if (first_index_set == 0) {
-                    first_index = 8 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                    first_index_set = 1;
-                } else {
-                    second_index = 8 + 48;
-                    tester_index = 0;
-                    memset(tester, 0, strlen(tester));
-                }         
-                break;
-            default:
-                memset(tester, 0, strlen(tester));
-                tester_index = 0;
-                goto first_char;
-                break;
-            }
-        } else {
-            tester_index = 0;
+            j++;
         }
 
-        printf("char: %c\ttester: %s\tfirst_index: %d\tsecond_index: %d\tfirst_index_set: %d\ttester_index: %d\n", buffer[i], tester, first_index-48, second_index-48, first_index_set, tester_index);
-        // one
-        // two
-        // three
-        // four
-        // five
-        // six
-        // seven
-        // eight
-        // nine
-        // zero
+        reverse(token);
 
+        positions_reverse[1] = strindex(token, eno);
+        positions_reverse[2] = strindex(token, owt);
+        positions_reverse[3] = strindex(token, eerht);
+        positions_reverse[4] = strindex(token, ruof);
+        positions_reverse[5] = strindex(token, evif);
+        positions_reverse[6] = strindex(token, xis);
+        positions_reverse[7] = strindex(token, neves);
+        positions_reverse[8] = strindex(token, thgie);
+        positions_reverse[9] = strindex(token, enin);
+        positions_reverse[0] = strindex(token, orez);
 
-        i++;
+        int n = 0;
+        int text_min = 10000;
+        while (n < 10) {
+            if (positions_reverse[n] < last_text_index) {
+                text_min = n;
+                last_text_index = positions_reverse[n];
+            }
+            n++;
+        }
+        last_text = text_min;
+
+        int m = 0;
+        while (m < strlen(token)) {
+            if (token[m] >= 48 && token[m] <= 57) {
+                last_num = token[m] - 48;
+                last_num_index = m;
+                break;
+            }
+            m++;
+        }
+
+        if (first_num_index < first_text_index) {
+            final_first = first_num;
+        } else {
+            final_first = first_text;
+        }
+
+        if (last_num_index < last_text_index) {
+            final_last = last_num;
+        } else {
+            final_last = last_text;
+        }
+
+        total_sum += final_first*10 + final_last;
+        token = strtok(NULL, delim);
     }
-
-    printf("total: %d", total_sum);
-
+    
+    printf("total: %d\n", total_sum);
 
     
     
